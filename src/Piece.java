a3932e61485b400e455d470e137f00b5bc3ca1e8
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-26 19:26:29 +0700

    add Piece model

---
+++ src/Piece.java
@@ -0,0 +1,15 @@
+public class Piece {
+    private Square location;
+
+    Piece(Square location) {
+        this.location = location;
+    }
+
+    public void setLocation(Square location) {
+        this.location = location;
+    }
+
+    public Square getLocation() {
+        return this.location;
+    }
+}

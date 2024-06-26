c859dc31d471b6db91945908b2b8f28800792eb7
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-26 19:24:03 +0700

    add Square model

---
+++ src/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+    private final String name;
+
+    Square(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}

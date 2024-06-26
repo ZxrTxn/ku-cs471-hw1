a0cd5408cfc47c2dc642358a4778e16e70f50af2
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-26 19:58:40 +0700

    add roll method

--- src/Die.java
+++ src/Die.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Die {
+    private static final int[] FACES = {1, 2, 3, 4, 5, 6};
+
     private int faceValue;
+
+    Die() {
+        this.faceValue = FACES[0];
+    }
+
+    public void roll() {
+        int randomIndex = new Random().nextInt(FACES.length);
+
+        this.faceValue = FACES[randomIndex];
+    }
 
     public int getFaceValue() {
         return this.faceValue;

2aa89e2318d1125ed17c92df27d8fa18d50525eb
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-27 08:33:29 +0700

    add getSquare method

--- src/Board.java
+++ src/Board.java
@@ -6,4 +6,11 @@ public class Board {
     Board(Set<Square> squares) {
         this.squares = squares;
     }
+
+    public Square getSquare(Square oldLoc, int fvTot) {
+        int startRefNumber = squares.stream().findFirst().get().getRefNumber();
+        int newRefInd = (oldLoc.getRefNumber() - startRefNumber + fvTot) % this.squares.size();
+
+        return squares.stream().filter(square -> (square.getRefNumber() == newRefInd + startRefNumber)).findFirst().get();
+    }
 }

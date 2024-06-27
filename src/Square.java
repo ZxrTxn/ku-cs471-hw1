de97be5928febf0ee9d98c2f309578052e41f30e
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-27 08:32:43 +0700

    implement Comparable interface

--- src/Square.java
+++ src/Square.java
@@ -1,11 +1,23 @@
-public class Square {
+public class Square implements Comparable<Square> {
+    private final int refNumber;
+
     private final String name;
 
-    Square(String name) {
+    Square(int refNumber, String name) {
+        this.refNumber = refNumber;
         this.name = name;
+    }
+
+    public int getRefNumber() {
+        return this.refNumber;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    @Override
+    public int compareTo(Square square) {
+        return Integer.compare(this.refNumber, square.refNumber);
     }
 }

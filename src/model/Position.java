f1ef52e8bb7e35e61ade912ed616af4eb3d20fd7
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-27 22:20:36 +0700

    add Position model

---
+++ src/model/Position.java
@@ -0,0 +1,32 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+package model;
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}

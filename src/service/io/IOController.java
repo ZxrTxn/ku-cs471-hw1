3cba507e47b03043e7142cf759d5eaab6f500ff9
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-29 00:09:12 +0700

    move Position class from model package to layout package

--- src/service/io/IOController.java
+++ src/service/io/IOController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 import java.util.Arrays;
 
-import model.Position;
+import util.property.layout.Position;
 
 public class IOController {
     private static final int SCREEN_WIDTH = 120;

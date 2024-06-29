2d76efc2415091ee6b35cb7366800532e7490e44
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-06-29 20:26:56 +0700

    add CharacterConstant class

---
+++ src/core/constant/CharacterConstant.java
@@ -0,0 +1,22 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+package core.constant;
+
+public final class CharacterConstant {
+    public static final char SPACE = ' ';
+    public static final char NEW_LINE = '\n';
+
+    public static final char VERTICAL_LINE = '|';
+    public static final char HORIZONTAL_LINE = '-';
+
+    public static final char VERTICAL_BORDER = VERTICAL_LINE;
+    public static final char HORIZONTAL_BORDER = HORIZONTAL_LINE;
+
+    public static final char BORDER_TOP = HORIZONTAL_BORDER;
+    public static final char BORDER_RIGHT = VERTICAL_BORDER;
+    public static final char BORDER_BOTTOM = HORIZONTAL_BORDER;
+    public static final char BORDER_LEFT = VERTICAL_BORDER;
+}
